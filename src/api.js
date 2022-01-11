eda46eb590d55c81d917a51a73343352d15114bb
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2022-01-11 21:00:55 +0100

    4.4 api changes

--- src/api.js
+++ src/api.js
@@ -23,54 +23,6 @@ const checkToken = async (accessToken) => {
   return result;
 };
 
-export const getEvents = async () => {
-  NProgress.start();
-
-  if (window.location.href.startsWith("http://localhost")) {
-    NProgress.done();
-    return mockData;
-  }
-
-
-  const token = await getAccessToken();
-
-  if (token) {
-    removeQuery();
-    const url = "https://wmozuka1rl.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
-    const result = await axios.get(url);
-    if (result.data) {
-      var locations = extractLocations(result.data.events);
-      localStorage.setItem("lastEvents", JSON.stringify(result.data));
-      localStorage.setItem("locations", JSON.stringify(locations));
-      NProgress.done();
-      return result.data.events;
-    }
-  }
-};
-
-export const getAccessToken = async () => {
-
-  const accessToken = localStorage.getItem('access_token');
-
-  const tokenCheck = accessToken && (await checkToken(accessToken));
-
-  if (!accessToken || tokenCheck.error) {
-    await localStorage.removeItem("access_token");
-    const searchParams = new URLSearchParams(window.location.search);
-    const code = await searchParams.get("code");
-    if (!code) {
-      const results = await axios.get(
-        "https://wmozuka1rl.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
-      );
-      const { authUrl } = results.data;
-      return (window.location.href = authUrl);
-    }
-    return code && getToken(code);
-  }
-  return accessToken;
-
-};
-
 const removeQuery = () => {
   if (window.history.pushState && window.location.pathname) {
     var newurl =
@@ -98,6 +50,56 @@ const getToken = async (code) => {
   access_token && localStorage.setItem("access_token", access_token);
 
   return access_token;
+};
+
+
+export const getEvents = async () => {
+  NProgress.start();
+
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }
+
+
+  const token = await getAccessToken();
+
+  if (token) {
+    removeQuery();
+    const url = "https://wmozuka1rl.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" + "/" + token;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+      NProgress.done();
+      return result.data.events;
+    }
+  }
+};
+
+
+export const getAccessToken = async () => {
+
+  const accessToken = localStorage.getItem('access_token');
+
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://wmozuka1rl.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+
 };
 
 
