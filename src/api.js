bfe29779f917af372101bf9867a9eb3214707893
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2022-01-11 22:49:00 +0100

    4.4 api link fix

--- src/api.js
+++ src/api.js
@@ -40,7 +40,7 @@ const removeQuery = () => {
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
-    'https://wmozuka1rl.execute-api.eu-central-1.amazonaws.com/dev/api/token/' + '/' + encodeCode
+    `https://wmozuka1rl.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`
   )
     .then((res) => {
       return res.json();
