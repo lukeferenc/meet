06b97a581085d50845a280bc18aba9f51a9a4d8a
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2022-01-25 02:43:36 +0100

    4.6 atatus adding

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +22,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.config('4ea31d6e1d6e44388da32ab1b8d33c6d').install();
