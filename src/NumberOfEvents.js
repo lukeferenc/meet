079cc982f4587433437bdf23a15fa6899ab3132b
lukeferenc <luke.theduke.of.cornwall@gmail.com>
2022-01-26 03:28:41 +0100

    4.7 adding alerts

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,28 +1,49 @@
 import React, { Component } from "react";
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
  state = {
     numberOfEvents: '32',
+    infoText: ''
   }
 
-  updateEventNumber = (event) => {
+
+
+  
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    if (value < 1 || value > 50) {
+      this.setState({
+        errorText: 'Please enter a number between 1 and 50',
+      })
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        errorText: '',
+      });
+    }
+    this.props.updateNumberOfEvents(event.target.value);
+  };
+
+  /*updateEventNumber = (event) => {
     const value = event.target.value;
     console.log(value, "value")
     this.setState({
       numberOfEvents: value,
     });
   this.props.updateNumberOfEvents(value)
-  }
+  } */
 
   render() {
     return (
       <div className="NumberOfEvents">
         <input
-          type="text"
+          type="number"
           className="number-events"
           value={this.state.numberOfEvents}
-          onChange={this.updateEventNumber}
+          onChange={(e) => this.handleInputChanged(e)}
         />
+        <ErrorAlert text={this.state.errorText} />
       </div>
     );
   }
